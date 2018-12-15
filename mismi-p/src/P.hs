e29022b1a8e310fda37c350fd8dadc2b2de65a6c
Nick Hibberd <nhibberd@gmail.com>
2018-12-15 14:12:52 -0800

    Clean up P

--- mismi-p/src/P.hs
+++ mismi-p/src/P.hs
@@ -1,87 +1,327 @@
 {-# LANGUAGE CPP #-}
-
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 module P (
-    module X
+  -- * Primitive types
+  -- ** Bool
+    Bool (..)
+  , bool
+  , (&&)
+  , (||)
+  , not
+  , otherwise
+  -- ** Char
+  , Char
+  -- ** Int
+  , Integer
+  , Int
+  , Int8
+  , Int16
+  , Int32
+  , Int64
+  -- ** Word
+  , Word64
+  -- ** Real
+  , fromIntegral
+  , fromRational
+
+  -- * Algebraic structures
+  -- ** Monoid
+  , Monoid (..)
+  , (<>)
+  -- ** Functor
+  , Functor (..)
+  , (<$>)
+  , ($>)
+  , void
+  , with
+  -- ** Bifunctor
+  , Bifunctor (..)
+  -- ** Applicative
+  , Applicative (..)
+  , (<**>)
+  -- ** Alternative
+  , Alternative (..)
+  , asum
+  -- ** Monad
+  , Monad (..)
+  , (=<<)
+  , join
+  , forM
+  , forM_
+  , mapM_
+  , when
+  , unless
+  -- ** MonadPlus
+  , MonadPlus (..)
+  , guard
+  , msum
+
+  -- * Data structures
+  -- ** Either
+  , Either (..)
+  , either
+  , note
+  -- ** Maybe
+  , Maybe (..)
+  , fromMaybe
+  , maybe
+  , hush
+  -- ** Tuple
+  , fst
+  , snd
+  , curry
+  , uncurry
+
+  -- * Typeclasses
+  -- ** Enum
+  , Enum (..)
+  -- ** Num
+  , Num (..)
+  -- ** Eq
+  , Eq (..)
+  -- ** Read
+  , Read (..)
+  , readEither
+  , readMaybe
+  -- ** Show
+  , Show (..)
+  -- *** ShowS
+  , ShowS
+  , showString
+  -- ** Foldable
+  , Foldable (..)
+  , for_
+  , all
+  -- ** Ord
+  , Ord (..)
+  , Ordering (..)
+  , comparing
+  -- ** Traversable
+  , Traversable (..)
+  , for
+  , traverse_
+
+  -- * Combinators
+  , id
+  , (.)
+  , ($)
+  , ($!)
+  , (&)
+  , const
+  , flip
+  , fix
+  , on
+  , seq
+
+  -- * System
+  -- ** IO
+  , IO
+  , FilePath
+
+  -- * Partial functions
+  , undefined
+  , error
+
+  -- * Debugging facilities
+  , trace
+  , traceM
+  , traceIO
+
+  -- * Text
+  , Text (..)
+
+  -- * Prelude
+  , Unsafe.Bounded
   ) where
 
-import           Prelude as X (
-                     Enum
-                   , Bounded
-                   , minBound
-                   , maxBound
-                   , ($!)
-                   , seq
-                   )
 
-import           P.Applicative as X
-import           P.Bifunctor.Trans as X
-import           P.Bool as X
-import           P.Either as X
-import           P.Foldable as X
-import           P.Functor as X
-import           P.Maybe as X
-import           P.Maybe.Strict as X
-import           P.Monad as X
-import           P.Monoid as X
-import           P.Ord as X
-import           P.List as X
-import           P.Function as X
-import           P.Debug as X
-import           P.Show as X
-import           Control.Applicative as X (
-                     Applicative(..)
-                   , Alternative(..)
-                   , Const(..)
-                   , WrappedMonad(..)
-                   , WrappedArrow(..)
-                   , ZipList(..)
-                   , (<**>)
-                   , liftA
-                   , liftA2
-                   , liftA3
-                   , optional
-                   )
-import           Control.DeepSeq as X (
-                     NFData(..)
-                   , ($!!)
-                   , deepseq
-                   , force
-                   )
-import           Data.Eq as X
-import           Data.Bifunctor as X (Bifunctor(..))
-import           Data.Bool as X
-import           Data.Char as X (Char)
-import           Data.List as X (
-                     intercalate
-                   , isPrefixOf
-                   , drop
-                   , splitAt
-                   , break
-                   , filter
-                   , reverse
-#if (__GLASGOW_HASKELL__ < 710)
-                   , length
-                   , null
+import           Control.Monad as Monad (
+           Monad (..)
+         , MonadPlus (..)
+         , (=<<)
+         , guard
+         , join
+         , msum
+         , forM
+         , forM_
+         , mapM_
+         , when
+         , unless
+         )
+import           Control.Applicative as Applicative (
+           Applicative (..)
+         , (<**>)
+         , Alternative (..)
+         , empty
+         )
+
+import           Data.Bifunctor as Bifunctor (
+           Bifunctor (..)
+         )
+import           Data.Bool as Bool (
+           Bool (..)
+         , bool
+         , (&&)
+         , (||)
+         , not
+         , otherwise
+         )
+import           Data.Char as Char (
+           Char
+         )
+import           Data.Either as Either (
+           Either (..)
+         , either
+         )
+import           Data.Foldable as Foldable (
+           Foldable (..)
+         , asum
+         , traverse_
+         , for_
+         , all
+         )
+import           Data.Function as Function (
+           id
+         , (.)
+         , ($)
+         , (&)
+         , const
+         , flip
+         , fix
+         , on
+         )
+import           Data.Functor as Functor (
+           Functor (..)
+         , (<$>)
+         , ($>)
+         , void
+         )
+import           Data.Eq as Eq (
+           Eq (..)
+         )
+import           Data.Int as Int (
+           Int
+         , Int8
+         , Int16
+         , Int32
+         , Int64
+         )
+import           Data.Maybe as Maybe (
+           Maybe (..)
+         , fromMaybe
+         , maybe
+         )
+import           Data.Monoid as Monoid (
+           Monoid (..)
+         , (<>)
+         )
+import           Data.Ord as Ord (
+           Ord (..)
+         , Ordering (..)
+         , comparing
+         )
+import           Data.Traversable as Traversable (
+           Traversable (..)
+         , for
+         )
+import           Data.Tuple as Tuple (
+           fst
+         , snd
+         , curry
+         , uncurry
+         )
+import           Data.Word as Word (
+           Word64
+         )
+
+import qualified Debug.Trace as Trace
+
+import           GHC.Real as Real (
+           fromIntegral
+         , fromRational
+         )
+#if MIN_VERSION_base(4,9,0)
+import           GHC.Stack (HasCallStack)
 #endif
-                   )
-import           Data.Maybe as X hiding (fromJust)
-import           Data.Either as X
-import           Data.Int as X
-import           Data.Ord as X
-import           Data.Tuple as X
-import           Data.Traversable as X
-import           Data.Text as X (Text)
-import           Data.Foldable as X hiding (
-                     foldr1
-                   , foldl1
-                   , maximum
-                   , maximumBy
-                   , minimum
-                   , minimumBy
-                   )
-import           GHC.Num as X
-import           GHC.Real as X
-import           GHC.Float as X
-import           Text.Show as X
-import           Text.Read as X (Read, reads, readMaybe, readEither)
-import           System.IO as X (FilePath, IO)
+
+import           Prelude as Prelude (
+           Enum (..)
+         , Num (..)
+         , Integer
+         , seq
+         , ($!)
+         )
+import qualified Prelude as Unsafe
+
+import           System.IO as IO (
+           FilePath
+         , IO
+         )
+
+import           Text.Read as Read (
+           Read (..)
+         , readEither
+         , readMaybe
+         )
+import           Text.Show as Show (
+           Show (..)
+         , ShowS
+         , showString
+         )
+import           Data.Text (Text)
+
+
+#if MIN_VERSION_base(4,9,0)
+undefined :: HasCallStack => a
+#else
+undefined :: a
+#endif
+undefined =
+  Unsafe.undefined
+{-# WARNING undefined "'undefined' is unsafe" #-}
+
+#if MIN_VERSION_base(4,9,0)
+error :: HasCallStack => [Char] -> a
+#else
+error :: [Char] -> a
+#endif
+error =
+  Unsafe.error
+{-# WARNING error "'error' is unsafe" #-}
+
+trace :: [Char] -> a -> a
+trace =
+  Trace.trace
+{-# WARNING trace "'trace' should only be used while debugging" #-}
+
+#if MIN_VERSION_base(4,9,0)
+traceM :: Applicative f => [Char] -> f ()
+#else
+traceM :: Monad m => [Char] -> m ()
+#endif
+traceM =
+  Trace.traceM
+{-# WARNING traceM "'traceM' should only be used while debugging" #-}
+
+traceIO :: [Char] -> IO ()
+traceIO =
+  Trace.traceIO
+{-# WARNING traceIO "'traceIO' should only be used while debugging" #-}
+
+with :: Functor f => f a -> (a -> b) -> f b
+with =
+  flip fmap
+{-# INLINE with #-}
+
+-- | Tag a 'Nothing'.
+note :: a -> Maybe b -> Either a b
+note a Nothing = Left a
+note _ (Just b) = Right b
+{-# INLINEABLE note #-}
+
+-- | Eliminate a 'Left'.
+hush :: Either a b -> Maybe b
+hush (Left _) = Nothing
+hush (Right b) = Just b
+{-# INLINEABLE hush #-}
